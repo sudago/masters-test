64c99f8ca49fc301516c7fbc1304538e4d373bf8
sudago <sudago22@gmail.com>
2022-12-06 03:12:07 +0900

    makeCircleArray() 기능을 분리함
    level2구현하기 위해 PlanetLocation 클래스에서도 사용하기 위함.

--- src/level1/MakeCircle.java
+++ src/level1/MakeCircle.java
@@ -2,7 +2,22 @@ package level1;
 
 import java.util.Scanner;
 
-public class PrintCircle {
+public class MakeCircle {
+    public static String[][] makeCircleArray(int circle) {
+        String[][] circleArray = new String[circle][circle];
+        int halfCircle = (circle - 1) / 2;
+        int evenNumber = circle % 2 == 0 ? 1 : 0;
+
+        for (int i = 0; i <= halfCircle; i++) { // 2차원 배열 채우기
+            int reverseRow = circle - 1 - i;
+
+            for (int j = (halfCircle - i); j <= (halfCircle + i + evenNumber); j++) { // 왼쪽 ~ 오른쪽 채우기
+                circleArray[i][j] = "*"; // 앞쪽
+                circleArray[reverseRow][j] = "*"; //뒷쪽
+            }
+        }
+        return circleArray;
+    }
     public static void main(String[] args) {
         System.out.println("1부터 80 사이의 정수를 입력해주세요.");
         System.out.println("원의 크기는?");
@@ -15,25 +30,14 @@ public class PrintCircle {
             circle = scanner.nextInt();
         }
 
-        String[][] circleArray = new String[circle][circle];
-        int halfCircle = (circle - 1) / 2;
-        int evenNumber = circle % 2 == 0 ? 1 : 0;
-
-        for (int i = 0; i <= halfCircle; i++) { // 2차원 배열 채우기
-            int reverseRow = circle - 1 - i;
-
-            circleArray[i][halfCircle - i] = "-";//앞 왼
-            circleArray[i][halfCircle + i + evenNumber] = "-";//앞 오
-            circleArray[reverseRow][halfCircle - i] = "-";//뒤 왼
-            circleArray[reverseRow][halfCircle + i + evenNumber] = "-";//뒤 오
-        }
+        String[][] circleArray = makeCircleArray(circle); // MakeCircle 클래스의 static 메서드 사용.
 
         for (String[] row: circleArray) { //출력
             for (String column : row) {
                 if (column == null) {
                     System.out.print(" ");
                 } else {
-                    System.out.print("-");
+                    System.out.print("*");
                 }
             }
             System.out.println("");
