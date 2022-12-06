488f0fdf6e063d49adb172b3142d62e4fd9a988e
sudago <sudago22@gmail.com>
2022-12-06 18:35:43 +0900

    빈 원을 만드는 기능 추가

--- src/level1/MakeCircle.java
+++ src/level1/MakeCircle.java
@@ -3,7 +3,8 @@ package level1;
 import java.util.Scanner;
 
 public class MakeCircle {
-    public static String[][] makeCircleArray(int circle) {
+    // 채워진 원 만들기
+    public static String[][] makeFilledCircleArray(int circle) {
         String[][] circleArray = new String[circle][circle];
         int halfCircle = (circle - 1) / 2;
         int evenNumber = circle % 2 == 0 ? 1 : 0;
@@ -18,6 +19,24 @@ public class MakeCircle {
         }
         return circleArray;
     }
+    // 빈 원 만들기
+    public static String[][] makeEmptyCircleArray(int circle) {
+        String[][] circleArray = new String[circle][circle];
+        int halfCircle = (circle - 1) / 2;
+        int evenNumber = circle % 2 == 0 ? 1 : 0;
+
+        for (int i = 0; i <= halfCircle; i++) { // 2차원 배열 채우기
+            int reverseRow = circle - 1 - i;
+
+            circleArray[i][halfCircle - i] = "-";//앞 왼
+            circleArray[i][halfCircle + i + evenNumber] = "-";//앞 오
+            circleArray[reverseRow][halfCircle - i] = "-";//뒤 왼
+            circleArray[reverseRow][halfCircle + i + evenNumber] = "-";//뒤 오
+        }
+        return circleArray;
+    }
+
+
     public static void main(String[] args) { // 4번 수정함.
         System.out.println("1부터 80 사이의 정수를 입력해주세요.");
         System.out.println("원의 크기는?");
@@ -30,14 +49,14 @@ public class MakeCircle {
             circle = scanner.nextInt();
         }
 
-        String[][] circleArray = makeCircleArray(circle); // MakeCircle 클래스의 static 메서드 사용.
+        String[][] circleArray = makeEmptyCircleArray(circle); // MakeCircle 클래스의 static 메서드 사용.
 
         for (String[] row: circleArray) { //출력
             for (String column : row) {
                 if (column == null) {
                     System.out.print(" ");
                 } else {
-                    System.out.print("*");
+                    System.out.print(column);
                 }
             }
             System.out.println("");
